47b9356053d435eade50c5d02a6f6e328898698f
LanUAriesEchos <91972966+LanUAriesEchos@users.noreply.github.com>
2024-08-22 16:46:53 -0400

    Update NavFoot.js

--- RS/NavFoot.js
+++ RS/NavFoot.js
@@ -1,12 +1,12 @@
 // Loads the Navbar and footer. Use <div id="navbar-placeholder"></div> to place the navbar into the page
 // Use <div id="footer-placeholder"></div> for the footer, Use this at the END OF <BODY> PERFERABLY ONE LINE ABOVE </BODY>!!!!!!
 // Load the navbar from the navbar.html file
-fetch('nav.html')
+fetch('https://www.thelanout.net/RS/nav.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar-placeholder').innerHTML = data;
     });
-    fetch('footer.html')
+    fetch('https://www.thelanout.net/RS/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-placeholder').innerHTML = data;
