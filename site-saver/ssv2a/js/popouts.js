a0e8d43d7477baceed664fa72f2e0e714d7224f8
LanUAriesEchos <91972966+LanUAriesEchos@users.noreply.github.com>
2024-11-27 22:14:31 -0500

    Update popouts.js

---
+++ site-saver/ssv2a/js/popouts.js
@@ -0,0 +1,4 @@
+const updatesButton = document.getElementById('updates-button');
+updatesButton.onclick = () => {
+window.open('updates.html', 'UpdatesPopup', 'width=600,height=400,scrollbars=yes,resizable=yes');
+};
