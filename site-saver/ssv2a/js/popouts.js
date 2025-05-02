26df3f2a2a72feeae14c098cf678de979c6eb50b
LanAriesEchos <91972966+LanUAriesEchos@users.noreply.github.com>
2025-05-02 09:59:52 -0400

    Delete site-saver/ssv2a directory

--- site-saver/ssv2a/js/popouts.js
+++
@@ -1,4 +0,0 @@
-const updatesButton = document.getElementById('updates-button');
-updatesButton.onclick = () => {
-window.open('updates.html', 'UpdatesPopup', 'width=600,height=400,scrollbars=yes,resizable=yes');
-};
